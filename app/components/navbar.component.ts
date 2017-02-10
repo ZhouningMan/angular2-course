ee6ea8dc5eb90179dc7c28728e6cf9616afec4ed
Linfeng <linfenglee321@gmail.com>
2017-02-09 22:11:46 -0500

    Added Nav bar

---
+++ app/components/navbar.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector:"navbar",
+    templateUrl: "app/components/templates/navbar.component.html"
+})
+export class NavBarComponent {
+
+}
