9d29ce0ae5ddc9a22da5a6183bc83182b4b2df6c
Linfeng <linfenglee321@gmail.com>
2017-02-09 22:42:28 -0500

    Setup single page application by adding the routes

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,9 +1,24 @@
 import {Component} from 'angular2/core';
 import {NavBarComponent} from "./navbar.component";
+import {HomeComponent} from "./home.component";
+import {UsersComponent} from "./users.component";
+import {PostsComponent} from "./posts.component";
+import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 
+@RouteConfig([
+    { path: '/', name: 'Home', component: HomeComponent }, //root path
+ 	{ path: '/users', name: 'Users', component: UsersComponent },
+ 	{ path: '/posts', name: 'Posts', component: PostsComponent },
+ 	{ path: '/*other', name: 'Other', redirectTo: ['Home'] }   //redirect non-existing path to home
+])
 @Component({
     selector: 'my-app',
-    template: '<navbar></navbar>',
-    directives: [NavBarComponent]
+    template: `
+        <navbar></navbar>
+        <div class="container">
+            <router-outlet></router-outlet>
+        </div>
+    `, //router-outlet is where real content is rendered
+    directives: [NavBarComponent, ROUTER_DIRECTIVES]
 })
 export class AppComponent { }
--- app/components/navbar.component.ts
+++ app/components/navbar.component.ts
@@ -1,8 +1,10 @@
 import {Component} from "angular2/core";
+import {ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
     selector:"navbar",
-    templateUrl: "app/components/templates/navbar.component.html"
+    templateUrl: "app/components/templates/navbar.component.html",
+    directives:[ROUTER_DIRECTIVES]
 })
 export class NavBarComponent {
 
